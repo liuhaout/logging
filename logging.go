1b86949c095499257ee962e7878dd9c95ddb2e8b
liuhaout <liuhaout@outlook.com>
2017-12-14 01:12:05 +0800

    the way of flush log content is modified,and the type of logger is value type

--- logging.go
+++ logging.go
@@ -8,14 +8,14 @@ import (
 	"strings"
 )
 
-// Log fields
+// log fields
 type Logging struct {
 	LogDir, LogName, LogPrefix string
 	LogSize, LogBackupNum      int64
 	LogFlags                   int
 }
 
-var Log *Logging
+var Logger Logging
 
 // init logfile config, or redefine by Log afterwards
 func init() {
@@ -23,9 +23,9 @@ func init() {
 	if err != nil {
 		log.Panic(err)
 	}
-	Log = &Logging{
+	Logger = Logging{
 		LogDir:    dir,
-		LogName:   "UnnamedLog.log",
+		LogName:   "Unnamed.log",
 		LogPrefix: "",
 
 		LogSize:      1024,
@@ -34,80 +34,90 @@ func init() {
 	}
 }
 
-// Log level
-func (l *Logging) EMEG(v interface{}) {
-	handle("EMEG", v)
-}
-func (l *Logging) ALERT(v interface{}) {
-	handle("ALERT", v)
-}
-func (l *Logging) CRIT(v interface{}) {
-	handle("CRIT", v)
-}
-func (l *Logging) ERR(v interface{}) {
-	handle("ERR", v)
-}
-func (l *Logging) WARNING(v interface{}) {
-	handle("WARNING", v)
-}
-func (l *Logging) NOTICE(v interface{}) {
-	handle("NOTICE", v)
-}
-func (l *Logging) INFO(v interface{}) {
-	handle("INFO", v)
-}
-func (l *Logging) DEBUG(v interface{}) {
-	handle("DEBUG", v)
+// log level
+func (logger Logging) EMEG(v interface{}) {
+	handle("EMEG", logger, v)
 }
 
-func handle(level string, v interface{}) {
-	flushlogFile()
-	f, err := os.OpenFile(fmt.Sprintf("%v/%v", Log.LogDir, Log.LogName), os.O_CREATE|os.O_APPEND, 0755)
+func (logger Logging) ALERT(v interface{}) {
+	handle("ALERT", logger, v)
+}
+func (logger Logging) CRIT(v interface{}) {
+	handle("CRIT", logger, v)
+}
+func (logger Logging) ERR(v interface{}) {
+	handle("ERR", logger, v)
+}
+func (logger Logging) WARNING(v interface{}) {
+	handle("WARNING", logger, v)
+}
+func (logger Logging) NOTICE(v interface{}) {
+	handle("NOTICE", logger, v)
+}
+func (logger Logging) INFO(v interface{}) {
+	handle("INFO", logger, v)
+}
+func (logger Logging) DEBUG(v interface{}) {
+	handle("DEBUG", logger, v)
+}
+
+func handle(level string, logger Logging, v interface{}) {
+	logFilePath := fmt.Sprintf("%v/%v", logger.LogDir, logger.LogName)
+	flushlogFile(logger, logFilePath)
+	f, err := os.OpenFile(logFilePath, os.O_CREATE|os.O_APPEND, 0755)
 	if err != nil {
 		log.Panic(err)
 	}
 	defer f.Close()
-	logger := log.New(f, Log.LogPrefix, Log.LogFlags)
-	logger.Printf("[%v] %v", level, v)
+	newlogger := log.New(f, logger.LogPrefix, logger.LogFlags)
+	newlogger.Printf("[%v] %v", level, v)
 }
 
-func flushlogFile() {
-	logFilePath := fmt.Sprintf("%v/%v", Log.LogDir, Log.LogName)
+func flushlogFile(logger Logging, logFilePath string) {
 	fileInfo, err := os.Stat(logFilePath)
+	dif := fileInfo.Size() - logger.LogSize
 	// logfile not exist
 	if err != nil {
 		return
 	}
 	// logfile exist
 	// expected logfile size
-	if fileInfo.Size() <= Log.LogSize {
+	if dif < 0 {
 		return
 	}
 	// logfile exist
 	// not expected logfile size
 	// logfile backup number = 1, will keep logfile size and flush content
-	if Log.LogBackupNum == 1 {
+	if logger.LogBackupNum == 1 {
 		data, err := ioutil.ReadFile(logFilePath)
 		if err != nil {
 			log.Panic(err)
 		}
-		tmp := strings.Join(strings.Split(string(data), "\n")[1:], "\n")
-		ioutil.WriteFile(logFilePath, []byte(tmp), 0755)
+		dataList := strings.Split(string(data), "\n")
+		if len(dataList) < 2 {
+			return
+		}
+		for i, _ := range dataList {
+			if int64(len(strings.Join(dataList[:i+1], "\n"))) > dif {
+				ioutil.WriteFile(logFilePath, []byte(strings.Join(dataList[i+1:], "\n")), 0755)
+				break
+			}
+		}
 		return
 	}
 	// logfile exist
 	// not expected logfile size
 	// logfile backup number > 1, will turn backup logfile
-	for i := Log.LogBackupNum - 2; i > -1; i-- {
+	for i := logger.LogBackupNum - 2; i > -1; i-- {
 		if i == 0 {
 			if _, err := os.Stat(logFilePath); err == nil {
-				os.Rename(logFilePath, fmt.Sprintf("%v/%v.%v", Log.LogDir, Log.LogName, i+1))
+				os.Rename(logFilePath, fmt.Sprintf("%v.%v", logFilePath, i+1))
 				os.Truncate(logFilePath, 0)
 				continue
 			}
 		}
-		if _, err := os.Stat(fmt.Sprintf("%v/%v.%v", Log.LogDir, Log.LogName, i)); err == nil {
-			os.Rename(fmt.Sprintf("%v/%v.%v", Log.LogDir, Log.LogName, i), fmt.Sprintf("%v/%v.%v", Log.LogDir, Log.LogName, i+1))
+		if _, err := os.Stat(fmt.Sprintf("%v.%v", logFilePath, i)); err == nil {
+			os.Rename(fmt.Sprintf("%v.%v", logFilePath, i), fmt.Sprintf("%v.%v", logFilePath, i+1))
 		}
 	}
 }
